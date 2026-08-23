ce438271b151e7be878e1e815548a1c838342463
Febin Francis <98605691+CodesbyFebin@users.noreply.github.com>
2026-08-23 07:21:51 +0530

    design: wrap app in persistent theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import { ThemeProvider } from './theme/ThemeContext.tsx';
 import './index.css';
 
 // The prerender shell gives crawlers meaningful HTML before JavaScript runs.
@@ -10,7 +11,9 @@ document.getElementById('prerender-shell')?.remove();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
 
